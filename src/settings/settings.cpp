8dfc8697b23cceabcdfde2ecdd17fa37007e3ec7
neel <diptoneelde@gmail.com>
2026-08-17 00:13:16 +0530

    Add default settings factory function

--- src/settings/settings.cpp
+++ src/settings/settings.cpp
@@ -1,6 +1,9 @@
 #include <string>
+
 struct UserSettings {
   bool emailNotifications;
   std::string theme;
   std::string language;
 };
+
+UserSettings defaultSettings() { return UserSettings{true, "light", "en"}; }
