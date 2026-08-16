f01f21d8638517128f97dd211398b1bf72069a11
neel <diptoneelde@gmail.com>
2026-08-16 23:52:48 +0530

    Add avatar file format validation

--- src/profile/avatar.cpp
+++ src/profile/avatar.cpp
@@ -2,3 +2,7 @@
 std::string defaultAvatarUrl(const std::string &username) {
   return "/avatars/" + username + ".png";
 }
+
+bool isValidAvatarFormat(const std::string &filename) {
+  return filename.size() > 4 && filename.substr(filename.size() - 4) == ".png";
+}
